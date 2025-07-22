af25a582d55188021e495b574973d6dbdd53b2da
Yeo changmin <minichanmgin@dgist.ac.kr>
2025-07-22 13:17:16 +0900

    revisse TwoMassMpc

--- TwoMassMPC.m
+++ TwoMassMPC.m
@@ -11,8 +11,10 @@ Bc = [0; 0; 0; 1;];
 Cc = [1 0 0 0];
 Dc = 0;
 dt = 0.01;
-Nc = 4;
-Np= 10;
+Nc = 3;
+Np= 60;
+R_wei_var = 0.1;
+R_wei = R_wei_var*eye(Nc);
 
 [Ap,Bp,Cp,Dp]=c2dm(Ac,Bc,Cc,Dc,dt);
 [Phi_Phi,Phi_F,Phi_R,A_e, B_e,C_e] =mpcgain(Ap,Bp,Cp,Nc,Np);
@@ -24,5 +26,19 @@ for i = 1:Num_s-1
     Ob = [Ob; Cp * Ap^i];
 end
 rank_Ob = rank(Ob);
+
+%%%%%  Closed Loop Control System %%%%%
+Ky_I = zeros(1,Nc);
+Ky_I(1) = 1;
+Ky_MPC = zeros(1,Nc);
+Ky_MPC(1) = 1;
+
+Ky = Ky_I*inv(Phi_Phi+R_wei)*Phi_R;
+Kmpc = Ky_MPC*inv(Phi_Phi+R_wei)*Phi_F;
+
+pole_mpc = eig(A_e-B_e*Kmpc);
+
+H = Phi_Phi+R_wei;
+con_num = cond(H);
 
 
